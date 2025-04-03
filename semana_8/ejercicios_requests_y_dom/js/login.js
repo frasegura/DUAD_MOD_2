7d1300a587b129f3c855f55fc81b17eedf10279a
frasegura <franciscosegura29@gmail.com>
2025-04-03 01:07:43 -0600

    semana_8 added

---
+++ semana_8/ejercicios_requests_y_dom/js/login.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const savedUser = localStorage.getItem("usuario");
+
+    if(savedUser){
+        window.location.href = "../html/user_data.html"; // cuando ya esta logueado
+    }
+
+    document.getElementById("log-in-forms").addEventListener("submit" , async function(event){
+        event.preventDefault();
+
+        const userId = document.getElementById("user_id").value.trim();
+        const password = document.getElementById("password").value.trim();
+        const URL = `https://api.restful-api.dev/objects/${userId}`;
+
+        if(!userId || !password){
+            alert("Porfavor ingrese su ID o su contrasena");
+            return;
+        }
+
+        try{
+            const response = await fetch(URL);
+
+            if (!response.ok) {
+                throw new Error("Usuario no encontrado.");
+            }
+
+            const data = await response.json();
+            console.log("Datos obtenidos de la API:", data);
+
+            if (data.data && data.data.password) {
+                if (data.data.password === password) {
+                    alert("Inicio de sesión exitoso");
+                    localStorage.setItem("usuario", JSON.stringify(data));
+                    window.location.href = "../html/user_data.html";
+                } else {
+                    alert("Contraseña incorrecta.");
+                }
+            } else {
+                alert("No se encontró la contraseña en la base de datos.");
+            }
+
+        }catch (error) {
+            console.error("Error en el inicio de sesión:", error);
+            alert("Usuario no encontrado o datos incorrectos.");
+        }
+    });
+});
+
+//id de usuario : ff808181932badb60195f2fa8f4e7cc4
---
+++ semana_8/ejercicios_requests_y_dom/js/registro.js
@@ -0,0 +1,51 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const usuario = localStorage.getItem("usuario");
+    if (usuario) {
+        window.location.href = "../html/user_data.html";
+    }
+});
+
+
+
+
+document.getElementById("my_forms").addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const last_name = document.getElementById("last_name").value.trim();
+    const address = document.getElementById("address").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const URL = "https://api.restful-api.dev/objects"
+
+    try{
+        const response = await fetch(URL,{
+            method: 'POST',
+            headers :  { "Content-Type": "application/json" },
+            body: JSON.stringify({name, 
+                                data : {last_name, 
+                                address, 
+                                password
+                                    }
+                                })
+        });
+
+        if (!response.ok) {
+            const errorText = await response.text();
+            throw new Error(`Error al registrar usuario: ${errorText}`);
+        }
+
+        const data = await response.json();
+
+        alert(`El usuario ${data.name} fue agregado correctamente, su ID es: ${data.id}`);
+        localStorage.setItem("usuario", JSON.stringify(data));
+        window.location.href = "../html/user_data.html";
+
+    }catch(error){
+        console.log("Ha ocurrido un error" , error.toString());
+    }
+})
+
+
+//ff808181932badb60195fa6efd140a48 , pwd: 1234
+
